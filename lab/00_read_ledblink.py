cc192b021c86a29f5f476780995d0c36b9556bfc
Stefan Mucha <stefan.mucha@hu-berlin.de>
2026-07-19 11:02:40 +0200

    helper to read out ledblinks from first video frames

---
+++ lab/00_read_ledblink.py
@@ -0,0 +1,88 @@
+# Program To Read video 
+# and Extract Frames 
+import cv2 
+import numpy as np
+import tkinter
+from tkinter import filedialog
+import glob
+import pandas as pd
+import re
+
+
+# LED pixelcoordinates
+x_s = 15
+x_e = 40
+y_s = 338
+y_e = 364
+
+# Video settings
+fps = 40
+
+# Time windows
+frames = 60*fps
+
+# Pick a directory
+root = tkinter.Tk()
+root.withdraw()
+vidpath = filedialog.askdirectory(title = "Select folder with video files")
+output_path = filedialog.askdirectory(title = 'Select folder for output of results')
+logpath = filedialog.askdirectory(title = "Select folder with log .txt files")
+vidlist = glob.glob(vidpath+'/*.avi')          # List all .avi files in data directory
+loglist = glob.glob(logpath+'/*[0-9].txt')          # List all .txt files in data directory
+
+fish_id = open(loglist[0], "r").readlines(30)[1][9:-1]
+#%%
+# Preallocate empty lists
+firstframe = []
+lastframe = []
+blinkduration = []
+timecol = []
+    
+for fname in vidlist:
+    # Path to video file 
+    vidObj = cv2.VideoCapture(fname) 
+    fps = int(vidObj.get(cv2.CAP_PROP_FPS))
+    n_frames = int(vidObj.get(cv2.CAP_PROP_FRAME_COUNT))
+    
+    # Used as counter variable 
+    count = 0
+      
+    # checks whether frames were extracted 
+    success = 1
+    
+    # empty lists
+    brightness = []
+    frame = []
+    
+    while success and count < frames: 
+      
+        success, image = vidObj.read() 
+        crop_img = image[y_s:y_e, x_s:x_e]   
+        brightness.append(np.mean(crop_img))
+        frame.append(count+1)
+        count += 1
+            
+            
+    threshold = (np.median(brightness) + max(brightness))/2
+    
+    blinkframes = np.argwhere(brightness > threshold)
+    firstframe.append(min(blinkframes)[0])
+    lastframe.append(max(blinkframes)[0])
+    blinkduration.append(len(blinkframes))
+    
+    # New: read time from .txt log file
+    logfile = [i for i in loglist if re.split("\\\\", fname)[-1][0:-8] in i][0]
+    logtext = open(logfile, "r").readline()
+    time_start = pd.to_datetime(logtext, format='%Y\\%m\\%d ; %H:%M:%S.%f\n')
+    timecol.append(time_start)
+    print(fname)
+
+    
+
+df = pd.DataFrame(
+    {'videoname':vidlist, 'firstframe': firstframe, 'lastframe': lastframe, \
+     'blinkduration': blinkduration, 'timestamp': timecol, 'id': fish_id, 'fps': fps}
+    )
+
+csv_name = output_path+fish_id+"_ledblinks_python.csv"
+df.to_csv(csv_name, index = False)
